8b71c22307d5c93e4c9e91293c1772d6d69366e6
Александр Ёлочка <elochkaan@gmail.com>
2025-09-14 19:58:14 +0700

    feat: add filtering for ips

--- lab_2_a.cpp
+++ lab_2_a.cpp
@@ -2,12 +2,13 @@
 #include <algorithm>
 #include <vector>
 #include <fstream>
+#include <iostream>
 
 bool
 compare(IP& a, IP& b)
 {   
-    decltype(auto) a_ip = a.getIP();
-    decltype(auto) b_ip = b.getIP();
+    const auto& a_ip = a.getIP();
+    const auto& b_ip = b.getIP();
 
     return a_ip > b_ip;
 }
@@ -25,6 +26,33 @@ main()
         i++;
     }
     
+    auto isFirstByteEqualOne = [](IP& a) -> bool{
+        return std::get<0>(a.getIP()) == 1;
+    };
+
+    auto is46and70 = [](IP& a) -> bool{
+        const auto& a_ip = a.getIP();
+        return std::get<0>(a_ip) == 46 && std::get<1>(a_ip) == 70;
+    };
+
+    auto is46any = [](IP& a) -> bool{
+        const auto& a_ip = a.getIP();
+        return std::get<0>(a_ip) == 46 ||
+               std::get<1>(a_ip) == 46 ||
+               std::get<2>(a_ip) == 46 ||
+               std::get<3>(a_ip) == 46;
+    };
+
     std::sort(ips.begin(), ips.end(), compare);
     for(IP& ip : ips) ip.print();
+    std::cout << "==========================================\n";
+
+    for(IP& ip : ips) if (isFirstByteEqualOne(ip)) ip.print();
+    std::cout << "==========================================\n";
+
+    for(IP& ip : ips) if (is46and70(ip)) ip.print();
+    std::cout << "==========================================\n";
+
+    for(IP& ip : ips) if (is46any(ip)) ip.print();
+    std::cout << "==========================================\n";
 }
