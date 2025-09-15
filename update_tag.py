4ed3be674931af7a8ce36ef574c057fd4963f6a9
Александр Ёлочка <elochkaan@gmail.com>
2025-09-15 15:54:16 +0700

    still fixing

--- update_tag.py
+++ update_tag.py
@@ -16,7 +16,7 @@ if current_tag == '':
     if last_tag == '':
         version = [1, 0, 0]
     else:
-        version = [int(i) for i in last_tag[1:].split('.')]
+        version = [int(i) for i in last_tag.split('.')]
 
     last_commit_with_last_tag = subprocess.run(['git', 'log', '-1', last_tag], capture_output=True, text=True).stdout.split('\n')[0].split()[1]
     print(f'Last commit of tag {last_tag} is {last_commit_with_last_tag}')
