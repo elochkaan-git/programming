39c27fa7e1196928d77f06152d803b3e9d2866c9
Александр Ёлочка <elochkaan@gmail.com>
2025-09-15 15:37:36 +0700

    for debugging

--- update_tag.py
+++ update_tag.py
@@ -3,15 +3,15 @@ import subprocess
 
 
 last_commit = subprocess.run(['git', 'rev-parse', 'HEAD'], capture_output=True, text=True).stdout.replace('\n', '')
-# print(f'Last commit is {last_commit}')
+print(f'Last commit is {last_commit}')
 
 current_tag = subprocess.run(['git', 'describe', '--contains', last_commit], capture_output=True, text=True).stdout.replace('\n', '')
-# print(f'Current tag is {current_tag if current_tag != '' else None}')
+print(f'Current tag is {current_tag if current_tag != '' else None}')
 
 if current_tag == '':
-    # print('Commit doesn\'t have a tag')
+    print('Commit doesn\'t have a tag')
     last_tag = subprocess.run(['git', 'describe', '--abbrev=0', '--tags'], capture_output=True, text=True).stdout.replace('\n', '')
-    # print(f'Last tag is {last_tag}')
+    print(f'Last tag is {last_tag}')
 
     if last_tag == '':
         version = [1, 0, 0]
@@ -19,7 +19,7 @@ if current_tag == '':
         version = [int(i) for i in last_tag[1:].split('.')]
 
     last_commit_with_last_tag = subprocess.run(['git', 'log', '-1', last_tag], capture_output=True, text=True).stdout.split('\n')[0].split()[1]
-    # print(f'Last commit of tag {last_tag} is {last_commit_with_last_tag}')
+    print(f'Last commit of tag {last_tag} is {last_commit_with_last_tag}')
     commits = subprocess.run(['git', 'log', f'{last_commit_with_last_tag}..{last_commit}', '--format=%B'], capture_output=True, text=True).stdout.split('\n\n')[::-1]
 
     for commit in commits:
